82806d97ebb1af079e7c1b1a991c77d3c83392d6
Matan Perelman <matan1008@gmail.com>
2018-08-14 23:44:30 +0300

    rp ack to ms tests added

--- tests/test_sms/test_rp_ack.py
+++ tests/test_sms/test_rp_ack.py
@@ -24,3 +24,24 @@ def test_parsing_rp_ack_to_n():
 def test_building_rp_ack_to_n():
     assert build(
         L3Message(3, protocol_discriminator.SMS, CpData(rp_mti.RP_ACK_MS_TO_N, RpAck(1)))) == b"\x39\x01\x02\x02\x01"
+
+
+def test_parsing_rp_ack_to_ms():
+    assert parse(b"\x29\x01\x02\x03\x04") == {
+        "transaction_identifier": 2,
+        "protocol_discriminator": protocol_discriminator.SMS,
+        "l3_protocol": {
+            "message_type": message_type.CP_DATA,
+            "cp_layer_protocol": {
+                "length_indicator": 2,
+                "spare": None,
+                "mti": rp_mti.RP_ACK_N_TO_MS,
+                "rp": {"message_reference": 4}
+            }
+        }
+    }
+
+
+def test_building_rp_ack_to_ms():
+    assert build(
+        L3Message(3, protocol_discriminator.SMS, CpData(rp_mti.RP_ACK_N_TO_MS, RpAck(1)))) == b"\x39\x01\x02\x03\x01"
