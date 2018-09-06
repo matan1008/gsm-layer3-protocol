8eb1d7041092cb51ea280275aa9d361d990a197a
Matan Perelman <matan1008@gmail.com>
2018-09-07 01:06:47 +0300

    docs

--- gsm_layer3_protocol/l3_message.py
+++ gsm_layer3_protocol/l3_message.py
@@ -5,6 +5,12 @@ from gsm_layer3_protocol.sms_protocol.sms import sms_struct
 
 class L3Message(Container):
     def __init__(self, transaction_identifier, protocol_discriminator, l3_protocol):
+        """
+        Container for a generic layer 3 message.
+        :param int transaction_identifier: A transaction identifier.
+        :param Enum protocol_discriminator: Any value of protocol_discriminator enum.
+        :param Container l3_protocol: Data relevant for the protocol specified.
+        """
         super().__init__(
             transaction_identifier=transaction_identifier,
             protocol_discriminator=protocol_discriminator,
